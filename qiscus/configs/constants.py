654fce25f945d3fad5c6bfbbe84937abea6b9765
andy-shi88 <shi88.andy@gmail.com>
2017-11-19 20:14:19 +0700

    register room endpoint routes

--- qiscus/configs/constants.py
+++ qiscus/configs/constants.py
@@ -4,4 +4,6 @@ ENDPOINTS = {
     'login_register': 'login_or_register',
     'user_profile': 'user_profile',
     'reset_user_token': 'reset_user_token',
+    'get_user_rooms': 'get_user_rooms',
+    'create_room': 'create_room'
 }
