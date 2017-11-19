93b8370365ce33086d1e9b00418a5edae9ba8bfc
andy-shi88 <shi88.andy@gmail.com>
2017-11-19 13:00:01 +0700

    update api route list

--- qiscus/configs/constants.py
+++ qiscus/configs/constants.py
@@ -1,6 +1,7 @@
-API_BASE_URI = '/api/v2/rest/'
+API_BASE_URI = '/api/v2/rest/' # noqa
 
 ENDPOINTS = {
     'login_register': 'login_or_register',
     'user_profile': 'user_profile',
+    'reset_user_token': 'reset_user_token',
 }
