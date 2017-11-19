01f4346b6f5071e88f23c5b8d4c5d915b49e28f5
andy-shi88 <shi88.andy@gmail.com>
2017-11-19 12:35:17 +0700

    update docstring

--- qiscus/api/contracts/api_base_interface.py
+++ qiscus/api/contracts/api_base_interface.py
@@ -8,11 +8,19 @@ class ApiBaseInterface(metaclass=ABCMeta):
 	"""
 
 	@abstractmethod
-	def get(self, endpoint, query=[]):
-		"""Send GET request to qiscus_base_url + $endpoint."""
+	def get(self, endpoint, query={}):
+		"""Send GET request to qiscus_base_url + $endpoint.
+		@params:
+			- endpoint, will be the endpoint for the request.
+			- query, will be the parameter query key-value dictionary.
+		"""
 		pass
 
 	@abstractmethod
 	def post(self, endpoint, payloads):
-		"""Send POST request to qiscus_base_url + $endpoint."""
+		"""Send POST request to qiscus_base_url + $endpoint.
+		@params:
+			- endpoint, will be the endpoint for the request.
+			- payloads, will be the json payload for the request.
+		"""
 		pass
